0467ec4d047aa80672af5862dc0ed1a2d149f57f
Jhenderson19 <69118664+Jhenderson19@users.noreply.github.com>
2021-05-07 23:37:38 -0400

    Update benchmark.js
    made timers easier to read

--- database/benchmark.js
+++ database/benchmark.js
@@ -1,11 +1,11 @@
 const Course = require('./classes/Course');
 
-console.time('generate1');
-console.time('generate100');
-console.time('generate1000');
-console.time('generate100000');
-console.time('generate1000000');
-console.time('generate10000000');
+console.time('Generate 1');
+console.time('Generate 100');
+console.time('Generate 1000');
+console.time('Generate 100000');
+console.time('Generate 1000000');
+console.time('Generate 10000000');
 
 for(var i = 1; i < 10000001; i++) {
   let x = new Course();
@@ -16,7 +16,7 @@ for(var i = 1; i < 10000001; i++) {
     case 100000:
     case 1000000:
     case 10000000:
-      console.timeEnd('generate'+i);
-      console.log(x);
+      console.timeEnd('Generate '+i);
+      //console.log(x);
   }
 }
