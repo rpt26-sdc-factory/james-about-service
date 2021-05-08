d21a1bd00f08117646796ec5a542d667e57fe4d1
Jhenderson19 <69118664+Jhenderson19@users.noreply.github.com>
2021-05-07 23:53:12 -0400

    Update benchmark.js
    moved around some timing code for hopefully more accurate results

--- database/benchmark.js
+++ database/benchmark.js
@@ -1,22 +1,18 @@
 const Course = require('./classes/Course');
 
 console.time('Generate 1');
-console.time('Generate 100');
-console.time('Generate 1000');
-console.time('Generate 100000');
-console.time('Generate 1000000');
+new Course();
+console.timeEnd('Generate 1');
+
 console.time('Generate 10000000');
+console.time('Generate 1000000');
+console.time('Generate 100000');
+console.time('Generate 10000');
+console.time('Generate 1000');
+console.time('Generate 100');
+console.time('Generate 10');
 
 for(var i = 1; i < 10000001; i++) {
   let x = new Course();
-  switch(i){
-    case 1:
-    case 100:
-    case 1000:
-    case 100000:
-    case 1000000:
-    case 10000000:
-      console.timeEnd('Generate '+i);
-      //console.log(x);
-  }
+  i.toString().length > (i-1).toString().length ? console.timeEnd('Generate '+i) : null;
 }
